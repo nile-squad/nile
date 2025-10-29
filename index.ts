f2524ba43c1b9330ac283e286cf9e2b01ee845c3
Hussseinkizz <hssnkizz@gmail.com>
2025-10-29 22:11:57 +0300

    feat(shared): proper create service engine exports

--- index.ts
+++ index.ts
@@ -3,7 +3,11 @@ export * as rest from './src/interfaces/rest';
 export * as rpc from './src/interfaces/rpc';
 export * as ws from './src/interfaces/ws';
 export * as logging from './src/logging';
-export * as servicesEngine from './src/services-engine';
+export {
+  createAction,
+  createServicesEngine,
+  ServicesEngine,
+} from './src/services-engine';
 export * as taskRunner from './src/task-runner';
 export * as types from './src/types';
 export * as utils from './src/utils';
